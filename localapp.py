1af8e3cb715a3025a774a4eedc7e148f9ebc2b8e
pastelmind <keepyourhonor@gmail.com>
2019-07-28 02:55:26 +0900

    Allow arbitrary paths and most HTTP methods
    This enables specifying arbitrary paths, as well as using all HTTP
    request methods supported by Cloud Functions.
    
    Note that when a deployed HTTP Function is invoked, the endpoint name is
    NOT available in any of the following attributes on the request object:
    
    - path: Starts with a slash (/) followed by subpath.
    - full_path: A question mark (?) plus the URL parameters appended to
      path.
    - script_root: Is empty.
    - url, base_url: Is the full URL for the request, but does not contain
      the method name.
    - url_root: Is the root URL (http://xxx.cloudfunctions.net/)
    
    Effectively, the Cloud Function behaves as though it is hosted at the
    root (/).

--- localapp.py
+++ localapp.py
@@ -4,8 +4,14 @@ from flask import Flask, request
 from main import on_request
 
 app = Flask(__name__)
+# Matches observed behavior of Cloud Functions
+app.url_map.strict_slashes = False
 
-@app.route('/', methods=['GET', 'POST'])
-def google_functions_endpoint():
+# Based on https://cloud.google.com/functions/docs/calling/http
+ALLOWED_METHODS = ['POST', 'PUT', 'GET', 'DELETE', 'OPTIONS']
+
+@app.route('/', methods=ALLOWED_METHODS)
+@app.route('/<path:_>', methods=ALLOWED_METHODS)
+def google_functions_endpoint(_=''):
     """Calls the Google Functions endpoint in `main.py`."""
     return on_request(request)
